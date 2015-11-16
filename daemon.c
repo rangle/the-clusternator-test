83ee9bafb08329bae69018df9b576eaed9f65ba8
Rafael Khan <raf.khan.f@gmail.com>
2015-11-16 16:35:26 -0500

    Init

---
+++ daemon.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+void make_daemon(void) {
+  pid_t pid;
+  pid_t sid;
+
+  pid = fork();
+  if(pid < 0) {
+    printf("Error: can not fork");
+    exit(-1);
+  } else if(pid > 0) {
+    printf("Started daemon.");
+    exit(0);
+  }
+
+  umask(0);
+
+
+  sid = setsid();
+  if (sid < 0) {
+    exit(-1);
+  }
+}
+
+int main(void) {
+  int i;
+  i = 0;
+
+  printf("STARTING\n");
+
+  while(1) {
+    printf("%d\n", i);
+    i++;
+    sleep(1);
+  }
+  
+  return 0;
+}
+
+// Commenting to force diff
